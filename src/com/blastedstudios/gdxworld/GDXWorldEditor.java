68320a7cb3c41d25fe773792a0189cb49a1003d4
narfman0 <narfman0@noob.blastedstudios.com>
2013-01-25 22:54:25 -0500

    -add program argument -l to check if we can load a file on startup

--- src/com/blastedstudios/gdxworld/GDXWorldEditor.java
+++ src/com/blastedstudios/gdxworld/GDXWorldEditor.java
@@ -1,15 +1,28 @@
 package com.blastedstudios.gdxworld;
 
+import java.io.File;
+
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.blastedstudios.gdxworld.ui.MainScreen;
+import com.blastedstudios.gdxworld.ui.worldeditor.WorldEditorScreen;
+import com.blastedstudios.gdxworld.world.GDXWorld;
 
 public class GDXWorldEditor extends Game {
+	private static File loadFile;
+	
 	@Override public void create () {
-		setScreen(new MainScreen(this));
+		if(loadFile.canRead())
+			setScreen(new WorldEditorScreen(this, GDXWorld.load(loadFile)));
+		else
+			setScreen(new MainScreen(this));
 	}
 	
 	public static void main (String[] argv) {
+		for(int i=0; i<argv.length; i+=2){
+			if(argv[i].equals("-l"))
+				loadFile = new File(argv[i+1]);
+		}
 		new LwjglApplication(new GDXWorldEditor(), "GDX World Editor", 1280, 1024, false);
 	}
 }
