84622add0e4eaa0abcb46904971233fa3e08c5f1
narfman0 <narfman0@noob.blastedstudios.com>
2013-03-23 01:22:56 -0400

    -add renderer

---
+++ src/com/blastedstudios/gdxworld/ui/GDXRenderer.java
@@ -0,0 +1,85 @@
+package com.blastedstudios.gdxworld.ui;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.files.FileHandle;
+import com.badlogic.gdx.graphics.Camera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+import com.blastedstudios.gdxworld.world.GDXBackground;
+import com.blastedstudios.gdxworld.world.GDXLevel;
+
+public class GDXRenderer {
+	private boolean drawBackground;
+	private Map<String, Texture> textureMap;
+	private SpriteBatch batch;
+	
+	public GDXRenderer(boolean drawBackground){
+		this.drawBackground = drawBackground;
+		textureMap = new HashMap<String, Texture>();
+		batch = new SpriteBatch();
+	}
+	
+	public void render(GDXLevel level, Camera camera){
+		batch.setProjectionMatrix(camera.combined);
+        batch.disableBlending();
+        batch.begin();
+        
+		if(drawBackground)
+			for(GDXBackground background : level.getBackgrounds()){
+				Texture texture = getTexture(background.getTexture());
+				if(texture != null){
+					float scale = Math.max(background.getDepth(), .001f);
+					Vector2 offset = new Vector2(texture.getWidth()/scale/2f,texture.getHeight()/scale/2f);
+					Vector2 xy = new Vector2(background.getCoordinates()).sub(offset);
+					xy.sub(camera.position.x, camera.position.y).div(scale);//parallax scrolling effect
+					batch.draw(texture, xy.x, xy.y, texture.getWidth()/scale, texture.getHeight()/scale);
+				}
+			}
+		batch.end();
+	}
+
+	public boolean isDrawBackground() {
+		return drawBackground;
+	}
+
+	public void setDrawBackground(boolean drawBackground) {
+		this.drawBackground = drawBackground;
+	}
+	
+	private Texture getTexture(String name){
+		if(!textureMap.containsKey(name)){
+			FileHandle file = find(Gdx.files.internal("data"), name);
+			if(file == null)
+				Gdx.app.error("GDXRenderer.render", "Texture " + name + " not found!");
+			else{
+				try{
+					textureMap.put(name, new Texture(file));
+					Gdx.app.log("GDXRenderer.render", "Added texture " + name);
+				}catch(Exception e){
+					Gdx.app.error("GDXRenderer.render", "Exception loading texture " + name);
+					e.printStackTrace();
+				}
+			}
+		}
+		return textureMap.get(name);
+	}
+	
+	/**
+	 * recursively find file in directory
+	 */
+	private FileHandle find(FileHandle handle, String name){
+		if(handle.isDirectory())
+			for(FileHandle childHandle : handle.list()){
+				FileHandle found = find(childHandle, name);
+				if(found != null)
+					return found;
+			}
+		else if(handle.name().equals(name))
+			return handle;
+		return null;
+	}
+}
