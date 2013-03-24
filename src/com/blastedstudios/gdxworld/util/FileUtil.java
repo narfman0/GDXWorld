9438b5a85efeb18d824dfd2a8c69b511a1c79c13
narfman0 <narfman0@noob.blastedstudios.com>
2013-03-24 15:56:58 -0400

    -Push some file code to fileutil

--- src/com/blastedstudios/gdxworld/util/FileUtil.java
+++ src/com/blastedstudios/gdxworld/util/FileUtil.java
@@ -1,5 +1,13 @@
 package com.blastedstudios.gdxworld.util;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+
 import com.badlogic.gdx.files.FileHandle;
 
 public class FileUtil {
@@ -16,5 +24,33 @@ public class FileUtil {
 		else if(path.name().equals(name))
 			return path;
 		return null;
+	}
+
+	public static void write(File file, Object obj, boolean compress){
+		try{
+			FileOutputStream fos = new FileOutputStream(file);
+			OutputStream os = /*compress ? new ZipOutputStream(fos) :*/ fos;
+			ObjectOutputStream oos = new ObjectOutputStream(os);
+			oos.writeObject(obj);
+			oos.close();
+			fos.close();
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+
+	public static Object read(File file, boolean compress){
+		Object object = null;
+		try{
+			FileInputStream fin = new FileInputStream(file);
+			InputStream in = /*compress ? ZipInputStream(fin) :*/ fin;
+			ObjectInputStream oin = new ObjectInputStream(in);
+			object = oin.readObject();
+			oin.close();
+			fin.close();
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		return object;
 	}
 }
--- src/com/blastedstudios/gdxworld/world/GDXWorld.java
+++ src/com/blastedstudios/gdxworld/world/GDXWorld.java
@@ -1,15 +1,12 @@
 package com.blastedstudios.gdxworld.world;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
 import com.badlogic.gdx.Gdx;
+import com.blastedstudios.gdxworld.util.FileUtil;
 
 public class GDXWorld implements Serializable{
 	private static final long serialVersionUID = 1L;
@@ -61,25 +58,12 @@ public class GDXWorld implements Serializable{
 				selectedFile.delete();
 				selectedFile.mkdirs();
 				for(int i=0; i<levels.size(); i++){
-					GDXLevel level = levels.get(i);
-					try{
-						FileOutputStream fileOut = new FileOutputStream(selectedFile + File.separator + i);
-						ObjectOutputStream out = new ObjectOutputStream(fileOut);
-						out.writeObject(level);
-						out.close();
-						fileOut.close();
-					}catch(Exception e){
-						e.printStackTrace();
-					}
+					FileUtil.write(new File(selectedFile + File.separator + i), levels.get(i), false);
 					Gdx.app.log("GDXWorld.save", "Successfully saved split " + selectedFile);
 				}
 			}else
 				try{
-					FileOutputStream fileOut = new FileOutputStream(selectedFile);
-					ObjectOutputStream out = new ObjectOutputStream(fileOut);
-					out.writeObject(this);
-					out.close();
-					fileOut.close();
+					FileUtil.write(selectedFile, this, false);
 					Gdx.app.log("GDXWorld.save", "Successfully saved non-split " + selectedFile);
 				}catch(Exception e){
 					e.printStackTrace();
@@ -97,23 +81,14 @@ public class GDXWorld implements Serializable{
 			Gdx.app.error("GDXWorld.load", "Cannot read file: " + selectedFile.getAbsolutePath());
 		else
 			try{
-				FileInputStream fin = new FileInputStream(selectedFile);
-				ObjectInputStream in = new ObjectInputStream(fin);
-				GDXWorld world = (GDXWorld) in.readObject();
-				in.close();
-				fin.close();
+				GDXWorld world = (GDXWorld) FileUtil.read(selectedFile, false);
 				Gdx.app.log("GDXWorld.load", "Successfully loaded non-split " + selectedFile);
 				return world;
 			}catch(Exception e){
 				try{
 					GDXWorld world = new GDXWorld();
-					for(File file : selectedFile.listFiles()){
-						FileInputStream fin = new FileInputStream(file);
-						ObjectInputStream in = new ObjectInputStream(fin);
-						world.levels.add((GDXLevel)in.readObject());
-						in.close();
-						fin.close();
-					}
+					for(File file : selectedFile.listFiles())
+						world.levels.add((GDXLevel)FileUtil.read(file, false));
 					Gdx.app.log("GDXWorld.load", "Successfully loaded split " + selectedFile);
 					return world;
 				}catch(Exception e1){
