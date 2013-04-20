4683d1e1bef5f76488ab24fc8a7325ba835de7a7
narfman0 <narfman0@noob.blastedstudios.com>
2013-04-20 13:28:46 -0400

    -Disabling parallax again

--- src/com/blastedstudios/gdxworld/ui/GDXRenderer.java
+++ src/com/blastedstudios/gdxworld/ui/GDXRenderer.java
@@ -33,9 +33,11 @@ public class GDXRenderer {
 			for(GDXBackground background : level.getBackgrounds()){
 				Texture texture = getTexture(background.getTexture());
 				if(texture != null){
-					float depth = Math.max(background.getDepth(), .001f);
+					//Disabling parallax for now
+					//float depth = Math.max(background.getDepth(), .001f);
 					Vector2 offset = new Vector2(texture.getWidth(),texture.getHeight()).scl(background.getScale()/2f);
-					Vector2 xy = toParallax(depth, background.getCoordinates(), camera).sub(offset);
+					//Vector2 xy = toParallax(depth, background.getCoordinates(), camera).sub(offset);
+					Vector2 xy = background.getCoordinates().cpy().sub(offset);
 					batch.draw(texture, xy.x, xy.y, texture.getWidth()*background.getScale(), 
 							texture.getHeight()*background.getScale());
 				}
