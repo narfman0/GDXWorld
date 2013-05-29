741bbe42a7af1612a21c51a84151010805ddf98f
Jon Robison <narfman0@yahoo.com>
2013-05-28 22:26:57 -0400

    -Drag according to current zoom with offensively arbitrary constant I'm sure will be refactored soon -Use Gdx logging -Change spacing a bit

--- src/com/blastedstudios/gdxworld/ui/MouseCameraScroller.java
+++ src/com/blastedstudios/gdxworld/ui/MouseCameraScroller.java
@@ -1,5 +1,6 @@
 package com.blastedstudios.gdxworld.ui;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputAdapter;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 
@@ -8,68 +9,68 @@ import com.badlogic.gdx.graphics.OrthographicCamera;
  * the specified mouse button is dragged.
  */
 public class MouseCameraScroller extends InputAdapter {
-    private OrthographicCamera camera;
-    private int button;
+	private OrthographicCamera camera;
+	private int button;
 
-    private boolean buttonHeld;
+	private boolean buttonHeld;
 
-    private int x0;
-    private int y0;
-    private float cx0;
-    private float cy0;
+	private int x0;
+	private int y0;
+	private float cx0;
+	private float cy0;
 
-    /**
-     * Sole constructor, specifying the camers to control and the button that
-     * dictates scrolling.
-     *
-     * @param camera the camera to control
-     * @param button the mouse button that causes scrolling; 0 for left, 1 for
-     * right, 2 for middle.
-     */
-    public MouseCameraScroller(OrthographicCamera camera, int button) {
-	if(camera == null) throw new IllegalArgumentException("Camera cannot be null!");
-	if(button < 0) throw new IllegalArgumentException("Button must be >= 0");
-	this.buttonHeld = false;
-	this.camera = camera;
-	this.button = button;
-	x0 = -1;
-	y0 = -1;
-    }
-
-    public boolean touchDown(int x, int y, int ptr, int buttonDown) {
-	if(buttonDown == button) {
-	    buttonHeld = true;
-	    x0 = x;
-	    y0 = y;
-	    cx0 = camera.position.x;
-	    cy0 = camera.position.y;
+	/**
+	 * Sole constructor, specifying the camers to control and the button that
+	 * dictates scrolling.
+	 *
+	 * @param camera the camera to control
+	 * @param button the mouse button that causes scrolling; 0 for left, 1 for
+	 * right, 2 for middle.
+	 */
+	public MouseCameraScroller(OrthographicCamera camera, int button) {
+		if(camera == null) throw new IllegalArgumentException("Camera cannot be null!");
+		if(button < 0) throw new IllegalArgumentException("Button must be >= 0");
+		this.buttonHeld = false;
+		this.camera = camera;
+		this.button = button;
+		x0 = -1;
+		y0 = -1;
 	}
-	return buttonHeld;
-    }
 
-    public boolean touchUp(int x, int y, int ptr, int buttonDown) {
-	if(buttonDown == button) {
-	    buttonHeld = false;
-	    x0 = -1;
-	    y0 = -1;
+	public boolean touchDown(int x, int y, int ptr, int buttonDown) {
+		if(buttonDown == button) {
+			buttonHeld = true;
+			x0 = x;
+			y0 = y;
+			cx0 = camera.position.x;
+			cy0 = camera.position.y;
+		}
+		return buttonHeld;
 	}
-	return buttonHeld;
-    }
 
-    public boolean touchDragged(int x, int y, int ptr) {
-	if(buttonHeld) {
-	    if(x0 != -1 && y0 != -1) {
-		System.err.println("X: " + x + "  Y: " + y);
-		int dx = (x - x0);
-		int dy = (y - y0);
-		camera.position.y = cy0 + dy;
-		camera.position.x = cx0 - dx;
-	    }
+	public boolean touchUp(int x, int y, int ptr, int buttonDown) {
+		if(buttonDown == button) {
+			buttonHeld = false;
+			x0 = -1;
+			y0 = -1;
+		}
+		return buttonHeld;
 	}
-	return buttonHeld;
-    }
 
-    public boolean isScrolling() {
-	return buttonHeld;
-    }
+	public boolean touchDragged(int x, int y, int ptr) {
+		if(buttonHeld) {
+			if(x0 != -1 && y0 != -1) {
+				Gdx.app.debug("MouseCameraScroller.touchDragged", "X: " + x + "  Y: " + y);
+				int dx = (int)((x - x0) * camera.zoom/50f);
+				int dy = (int)((y - y0) * camera.zoom/50f);
+				camera.position.y = cy0 + dy;
+				camera.position.x = cx0 - dx;
+			}
+		}
+		return buttonHeld;
+	}
+
+	public boolean isScrolling() {
+		return buttonHeld;
+	}
 }
