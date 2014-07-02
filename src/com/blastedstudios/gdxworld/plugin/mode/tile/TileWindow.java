72be0fb910494447ef45f9f050c7ec50a851250d
narfman0 <narfman0@gmail.com>
2014-07-02 17:50:16 -0400

    -Commenting unused functionality. This was bothering me way too much.

--- src/com/blastedstudios/gdxworld/plugin/mode/tile/TileWindow.java
+++ src/com/blastedstudios/gdxworld/plugin/mode/tile/TileWindow.java
@@ -5,10 +5,10 @@ import com.blastedstudios.gdxworld.ui.AbstractWindow;
 import com.blastedstudios.gdxworld.world.shape.GDXPolygon;
 
 public class TileWindow extends AbstractWindow {
-	private PaletteTile tile;
+//	private PaletteTile tile;
 	
 	public TileWindow(final Skin skin, final TileMode tileMode, final GDXPolygon tilePolygon) {
 		super("Tile Editor", skin);
-		this.tile = tile;
+//		this.tile = tile;
 	}
 }
