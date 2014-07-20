d0b1dc781311db9ceb913d55d10d9588c46d520c
narfman0 <narfman0@gmail.com>
2014-07-20 01:09:17 -0400

    -Add some fields to physics manifestation

--- src/com/blastedstudios/gdxworld/plugin/quest/manifestation/physics/PhysicsManifestation.java
+++ src/com/blastedstudios/gdxworld/plugin/quest/manifestation/physics/PhysicsManifestation.java
@@ -9,31 +9,40 @@ import com.blastedstudios.gdxworld.world.quest.manifestation.AbstractQuestManife
 
 public class PhysicsManifestation extends AbstractQuestManifestation{
 	private static final long serialVersionUID = 1L;
-	public static PhysicsManifestation DEFAULT = new PhysicsManifestation("Name",new Vector2(),BodyType.StaticBody,0);
+	public static PhysicsManifestation DEFAULT = new PhysicsManifestation();
 	/**
 	 * Name of physics object on which we execute tweaks
 	 */
-	private String name;
+	private String name = "Name";
 	/**
 	 * Impulse to be executed on named physics object
 	 */
-	private Vector2 impulse;
-	/**
+	private Vector2 impulse = new Vector2(), position = new Vector2(), velocity = new Vector2();
+	/*
 	 * Changing body type, for instance, unlocking door
 	 */
-	private BodyType type;
+	private BodyType type = BodyType.StaticBody;
 	/**
 	 * Provide torque, likely to turn a wheel or similar
 	 */
-	private float torque;
+	private float torque, angle;
+	private boolean hasPosition, hasVelocity, hasAngle;
 	
 	public PhysicsManifestation(){}
 	
-	public PhysicsManifestation(String name, Vector2 impulse, BodyType type, float torque){
+	public PhysicsManifestation(String name, Vector2 impulse, BodyType type, float torque, 
+			boolean hasPosition, Vector2 position, boolean hasVelocity, Vector2 velocity,
+			boolean hasAngle, float angle){
 		this.name = name;
 		this.impulse = impulse;
 		this.type = type;
 		this.torque = torque;
+		this.hasPosition = hasPosition;
+		this.position = position;
+		this.hasVelocity = hasVelocity;
+		this.velocity = velocity;
+		this.hasAngle = hasAngle;
+		this.angle = angle;
 	}
 
 	@Override public CompletionEnum execute() {
@@ -43,6 +52,10 @@ public class PhysicsManifestation extends AbstractQuestManifestation{
 			return CompletionEnum.COMPLETED;
 		}
 		body.applyLinearImpulse(impulse, body.getPosition(),true);
+		if(hasVelocity)
+			body.setLinearVelocity(velocity);
+		if(hasPosition)
+			body.setTransform(position, hasAngle ? angle : body.getAngle());
 		body.setType(type);
 		body.applyTorque(torque,true);
 		return CompletionEnum.COMPLETED;
@@ -81,11 +94,60 @@ public class PhysicsManifestation extends AbstractQuestManifestation{
 	}
 
 	@Override public AbstractQuestManifestation clone() {
-		return new PhysicsManifestation(name, impulse, type, torque);
+		return new PhysicsManifestation(name, impulse, type, torque, 
+				hasPosition, position, hasVelocity, velocity, hasAngle, angle);
 	}
 
 	@Override public String toString() {
 		return "[PhysicsManifestation: name:" + name + " impulse:" + impulse + 
 				" type:" + type + " torque:" + torque + "]";
+	}
+
+	public Vector2 getPosition() {
+		return position;
+	}
+
+	public void setPosition(Vector2 position) {
+		this.position = position;
+	}
+
+	public Vector2 getVelocity() {
+		return velocity;
+	}
+
+	public void setVelocity(Vector2 velocity) {
+		this.velocity = velocity;
+	}
+
+	public boolean isHasPosition() {
+		return hasPosition;
+	}
+
+	public void setHasPosition(boolean hasPosition) {
+		this.hasPosition = hasPosition;
+	}
+
+	public boolean isHasVelocity() {
+		return hasVelocity;
+	}
+
+	public void setHasVelocity(boolean hasVelocity) {
+		this.hasVelocity = hasVelocity;
+	}
+
+	public float getAngle() {
+		return angle;
+	}
+
+	public void setAngle(float angle) {
+		this.angle = angle;
+	}
+
+	public boolean isHasAngle() {
+		return hasAngle;
+	}
+
+	public void setHasAngle(boolean hasAngle) {
+		this.hasAngle = hasAngle;
 	}
 }
--- src/com/blastedstudios/gdxworld/plugin/quest/manifestation/physics/PhysicsManifestationTable.java
+++ src/com/blastedstudios/gdxworld/plugin/quest/manifestation/physics/PhysicsManifestationTable.java
@@ -1,17 +1,20 @@
 package com.blastedstudios.gdxworld.plugin.quest.manifestation.physics;
 
 import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
+import com.badlogic.gdx.scenes.scene2d.ui.CheckBox;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.List;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextField;
 import com.blastedstudios.gdxworld.plugin.mode.quest.ManifestationTable;
 import com.blastedstudios.gdxworld.ui.leveleditor.VertexTable;
 import com.blastedstudios.gdxworld.world.quest.manifestation.AbstractQuestManifestation;
 
 public class PhysicsManifestationTable extends ManifestationTable {
-	private final TextField torqueText, nameText;
-	private final VertexTable impulseTable;
+	private final TextField torqueText, nameText, angleText;
+	private final VertexTable impulseVertexTable, positionVertexTable, velocityVertexTable;
+	private final CheckBox hasPositionCheckbox, hasVelocityCheckbox, hasAngleCheckbox;
 	private final List<BodyType> bodyTypeList;
 	private final PhysicsManifestation manifestation;
 	
@@ -19,21 +22,47 @@ public class PhysicsManifestationTable extends ManifestationTable {
 		super(skin);
 		this.manifestation = manifestation;
 		nameText = new TextField(manifestation.getName(), skin);
-		nameText.setMessageText("<name>");
+		nameText.setMessageText("<body name>");
 		torqueText = new TextField(manifestation.getTorque()+"", skin);
 		torqueText.setMessageText("<torque>");
-		impulseTable = new VertexTable(manifestation.getImpulse(), skin, null);
+		impulseVertexTable = new VertexTable(manifestation.getImpulse(), skin, null);
 		bodyTypeList = new List<BodyType>(skin);
 		bodyTypeList.setItems(BodyType.values());
 		bodyTypeList.setSelectedIndex(manifestation.getType().getValue());
-		add(new Label("Name", skin));
+		Table positionTable = new Table(), velocityTable = new Table(), angleTable = new Table();
+		hasPositionCheckbox = new CheckBox("Has Position", skin);
+		hasPositionCheckbox.setChecked(manifestation.isHasPosition());
+		hasVelocityCheckbox = new CheckBox("Has Velocity", skin);
+		hasVelocityCheckbox.setChecked(manifestation.isHasVelocity());
+		hasAngleCheckbox = new CheckBox("Has Angle", skin);
+		hasAngleCheckbox.setChecked(manifestation.isHasAngle());
+		positionVertexTable = new VertexTable(manifestation.getPosition(), skin);
+		velocityVertexTable = new VertexTable(manifestation.getVelocity(), skin);
+		angleText = new TextField(manifestation.getAngle()+"", skin);
+		
+		add(new Label("Body Name", skin));
 		add(nameText);
 		row();
 		add(new Label("Torque: ", skin));
 		add(torqueText);
 		row();
 		add(new Label("Impulse: ", skin));
-		add(impulseTable);
+		add(impulseVertexTable);
+		row();
+		positionTable.add(hasPositionCheckbox);
+		positionTable.add(new Label(" Position: ", skin));
+		positionTable.add(positionVertexTable);
+		add(positionTable).colspan(2);
+		row();
+		velocityTable.add(hasVelocityCheckbox);
+		velocityTable.add(new Label(" Velocity: ", skin));
+		velocityTable.add(velocityVertexTable);
+		add(velocityTable).colspan(2);
+		row();
+		angleTable.add(hasAngleCheckbox);
+		angleTable.add(new Label(" Angle: ", skin));
+		angleTable.add(angleText);
+		add(angleTable).colspan(2);
 		row();
 		add(new Label("Body Type: ", skin));
 		add(bodyTypeList);
@@ -42,8 +71,14 @@ public class PhysicsManifestationTable extends ManifestationTable {
 	@Override public AbstractQuestManifestation apply() {
 		manifestation.setTorque(Float.parseFloat(torqueText.getText()));
 		manifestation.setName(nameText.getText());
-		manifestation.setImpulse(impulseTable.getVertex());
+		manifestation.setImpulse(impulseVertexTable.getVertex());
 		manifestation.setType(bodyTypeList.getSelected());
+		manifestation.setHasAngle(hasAngleCheckbox.isChecked());
+		manifestation.setHasPosition(hasPositionCheckbox.isChecked());
+		manifestation.setHasVelocity(hasVelocityCheckbox.isChecked());
+		manifestation.setPosition(positionVertexTable.getVertex());
+		manifestation.setVelocity(velocityVertexTable.getVertex());
+		manifestation.setAngle(Float.parseFloat(angleText.getText()));
 		return manifestation;
 	}
 }
