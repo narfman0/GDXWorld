f3e7bd897f3210fe52f08c643bab33f1733e6094
narfman0 <narfman0@gmail.com>
2014-10-03 00:04:07 -0400

    -Make dialog container much bigger

--- src/com/blastedstudios/gdxworld/plugin/quest/manifestation/dialog/DialogManifestationTable.java
+++ src/com/blastedstudios/gdxworld/plugin/quest/manifestation/dialog/DialogManifestationTable.java
@@ -2,25 +2,28 @@ package com.blastedstudios.gdxworld.plugin.quest.manifestation.dialog;
 
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextArea;
 import com.badlogic.gdx.scenes.scene2d.ui.TextField;
 import com.blastedstudios.gdxworld.plugin.mode.quest.ManifestationTable;
 import com.blastedstudios.gdxworld.world.quest.manifestation.AbstractQuestManifestation;
 
 public class DialogManifestationTable extends ManifestationTable {
-	private final TextField dialogText, originText, typeText;
+	private final TextArea dialogText;
+	private final TextField originText, typeText;
 	private final DialogManifestation manifestation;
 	
 	public DialogManifestationTable(Skin skin, DialogManifestation manifestation) {
 		super(skin);
 		this.manifestation = manifestation;
-		dialogText = new TextField(manifestation.getDialog(), skin);
+		dialogText = new TextArea(manifestation.getDialog(), skin);
 		dialogText.setMessageText("<dialog text>");
 		originText = new TextField(manifestation.getOrigin(), skin);
 		originText.setMessageText("<origin text>");
 		typeText = new TextField(manifestation.getType(), skin);
 		typeText.setMessageText("<type text>");
 		add(new Label("Dialog: ", skin));
-		add(dialogText);
+		add(dialogText).colspan(3).minSize(350f, 100f);
+		row();
 		add(new Label("Origin: ", skin));
 		add(originText);
 		add(new Label("Type: ", skin));
