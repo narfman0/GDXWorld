3c5de2d6c8813bb69807e294824093bc538de2f3
narfman0 <narfman0@gmail.com>
2014-10-30 22:41:10 -0400

    -Light refactoring, looking to maybe move meshes

---
+++ src/com/blastedstudios/gdxworld/util/MaskTextureStruct.java
@@ -0,0 +1,50 @@
+package com.blastedstudios.gdxworld.util;
+
+import com.badlogic.gdx.graphics.Camera;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Mesh;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.glutils.ShaderProgram;
+
+public class MaskTextureStruct {
+	private final static String FRAGMENT_SHADER = 
+			"#ifdef GL_ES \n" + 
+			"precision mediump float; \n" + 
+			"#endif \n" + 
+			"varying vec4 v_color; \n" + 
+			"varying vec2 v_texCoords; \n" + 
+			"uniform sampler2D u_texture; \n" + 
+			"void main(){ \n" + 
+			"  gl_FragColor = v_color * texture2D(u_texture, v_texCoords); \n" + 
+			"}";
+		private final static String VERTEX_SHADER = 
+			"attribute vec4 a_position; \n" + 
+			"attribute vec4 a_color; \n" + 
+			"attribute vec2 a_texCoord0; \n" + 
+			"uniform mat4 u_worldView; \n" + 
+			"varying vec4 v_color; \n" + 
+			"varying vec2 v_texCoords; \n" + 
+			"void main() { \n" + 
+			"  v_color = vec4(1, 1, 1, 1); \n" + 
+			"  v_texCoords = a_texCoord0; \n" + 
+			"  gl_Position = u_worldView * a_position; \n" + 
+			"}";
+	static final ShaderProgram SHADER = new ShaderProgram(VERTEX_SHADER, FRAGMENT_SHADER);
+	public final Mesh mesh;
+	public final Texture texture;
+
+	public MaskTextureStruct(Mesh mesh, Texture texture){
+		this.mesh = mesh;
+		this.texture = texture;
+	}
+	
+	public void render(Camera camera){
+		// is better to batch this shader call, so SHADER is not private
+		SHADER.begin();
+		SHADER.setUniformMatrix("u_worldView", camera.combined);
+		SHADER.setUniformi("u_texture", 0);
+		texture.bind();
+		mesh.render(SHADER, GL20.GL_TRIANGLES);
+		SHADER.end();
+	}
+}
--- src/com/blastedstudios/gdxworld/util/TiledMeshRenderer.java
+++ src/com/blastedstudios/gdxworld/util/TiledMeshRenderer.java
@@ -9,7 +9,6 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Mesh;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.VertexAttribute;
-import com.badlogic.gdx.graphics.glutils.ShaderProgram;
 import com.badlogic.gdx.math.Vector2;
 import com.blastedstudios.gdxworld.math.EarClippingTriangulator;
 import com.blastedstudios.gdxworld.ui.GDXRenderer;
@@ -18,41 +17,20 @@ import com.blastedstudios.gdxworld.world.shape.GDXPolygon;
 public class TiledMeshRenderer {
 	private final LinkedList<MaskTextureStruct> maskTextures = new LinkedList<>();
 	private final static float UV_SCALE = Properties.getFloat("tiled.uv.scale", 10f);
-	private final static String FRAGMENT_SHADER = 
-		"#ifdef GL_ES \n" + 
-		"precision mediump float; \n" + 
-		"#endif \n" + 
-		"varying vec4 v_color; \n" + 
-		"varying vec2 v_texCoords; \n" + 
-		"uniform sampler2D u_texture; \n" + 
-		"void main(){ \n" + 
-		"  gl_FragColor = v_color * texture2D(u_texture, v_texCoords); \n" + 
-		"}";
-	private final static String VERTEX_SHADER = 
-		"attribute vec4 a_position; \n" + 
-		"attribute vec4 a_color; \n" + 
-		"attribute vec2 a_texCoord0; \n" + 
-		"uniform mat4 u_worldView; \n" + 
-		"varying vec4 v_color; \n" + 
-		"varying vec2 v_texCoords; \n" + 
-		"void main() { \n" + 
-		"  v_color = vec4(1, 1, 1, 1); \n" + 
-		"  v_texCoords = a_texCoord0; \n" + 
-		"  gl_Position = u_worldView * a_position; \n" + 
-		"}";
-	private final ShaderProgram shader;
 
 	public TiledMeshRenderer(GDXRenderer gdxRenderer, Collection<GDXPolygon> polygons){
-		shader = new ShaderProgram(VERTEX_SHADER, FRAGMENT_SHADER);
 		for(GDXPolygon polygon : polygons)
-			if(polygon.isRepeatable() && polygon.getResource() != null && !polygon.getResource().isEmpty()){
-				EarClippingTriangulator triangulator = new EarClippingTriangulator();
-				List<Vector2> verticesAbsolute = triangulator.computeTriangles(polygon.getVerticesAbsolute());
-				Mesh mesh = new Mesh(true, verticesAbsolute.size(), 0, VertexAttribute.Position(), VertexAttribute.TexCoords(0));
-				Texture texture = gdxRenderer.getTexture(polygon.getResource());
-				mesh.setVertices(convert(verticesAbsolute, polygon.getCenter()));
-				maskTextures.add(new MaskTextureStruct(mesh, texture));
-			}
+			if(polygon.isRepeatable() && polygon.getResource() != null && !polygon.getResource().isEmpty())
+				maskTextures.add(buildMesh(gdxRenderer, polygon));
+	}
+	
+	public static MaskTextureStruct buildMesh(GDXRenderer gdxRenderer, GDXPolygon polygon){
+		EarClippingTriangulator triangulator = new EarClippingTriangulator();
+		List<Vector2> verticesAbsolute = triangulator.computeTriangles(polygon.getVerticesAbsolute());
+		Mesh mesh = new Mesh(true, verticesAbsolute.size(), 0, VertexAttribute.Position(), VertexAttribute.TexCoords(0));
+		Texture texture = gdxRenderer.getTexture(polygon.getResource());
+		mesh.setVertices(convert(verticesAbsolute, polygon.getCenter()));
+		return new MaskTextureStruct(mesh, texture);
 	}
 
 	private static float[] convert(final List<Vector2> vertices, final Vector2 center){
@@ -68,23 +46,13 @@ public class TiledMeshRenderer {
 	}
 
 	public void render(Camera camera){
-		shader.begin();
-		shader.setUniformMatrix("u_worldView", camera.combined);
-		shader.setUniformi("u_texture", 0);
+		MaskTextureStruct.SHADER.begin();
+		MaskTextureStruct.SHADER.setUniformMatrix("u_worldView", camera.combined);
+		MaskTextureStruct.SHADER.setUniformi("u_texture", 0);
 		for(MaskTextureStruct struct : maskTextures){
 			struct.texture.bind();
-			struct.mesh.render(shader, GL20.GL_TRIANGLES);
+			struct.mesh.render(MaskTextureStruct.SHADER, GL20.GL_TRIANGLES);
 		}
-		shader.end();
-	}
-
-	private class MaskTextureStruct{
-		public final Mesh mesh;
-		public final Texture texture;
-
-		public MaskTextureStruct(Mesh mesh, Texture texture){
-			this.mesh = mesh;
-			this.texture = texture;
-		}
+		MaskTextureStruct.SHADER.end();
 	}
 }
