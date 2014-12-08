d6ef931ff29469e6d9268bd833af504480097a37
Jon Robison <narfman0@gmail.com>
2014-12-07 23:49:51 -0500

    -Add omitted max length, ya know, the whole point of a rope joint

--- src/com/blastedstudios/gdxworld/plugin/mode/joint/window/rope/RopeWindow.java
+++ src/com/blastedstudios/gdxworld/plugin/mode/joint/window/rope/RopeWindow.java
@@ -2,8 +2,8 @@ package com.blastedstudios.gdxworld.plugin.mode.joint.window.rope;
 
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.JointDef.JointType;
-import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextField;
 import com.blastedstudios.gdxworld.plugin.mode.joint.BaseJointWindow;
 import com.blastedstudios.gdxworld.plugin.mode.joint.JointMode;
 import com.blastedstudios.gdxworld.ui.leveleditor.VertexTable;
@@ -12,13 +12,19 @@ import com.blastedstudios.gdxworld.world.joint.RopeJoint;
 class RopeWindow extends BaseJointWindow {
 	private final VertexTable centerTable;
 	private final RopeJoint joint;
+	private final TextField maxLengthField;
 
 	public RopeWindow(Skin skin, JointMode mode, RopeJoint joint) {
 		super("Rope Editor", skin, JointType.RopeJoint, mode, joint);
 		this.joint = joint;
 		centerTable = new VertexTable(joint.getCenter(), skin);
-		add(new Label("Anchor: ", skin));
+		maxLengthField = new TextField(joint.getMaxLength()+"", skin);
+		maxLengthField.setMessageText("<max length between bodies>");
+		add("Center: ");
 		add(centerTable);
+		row();
+		add("Max Length: ");
+		add(maxLengthField);
 		row();
 		add(createControlTable()).colspan(2);
 		pack();
@@ -27,6 +33,7 @@ class RopeWindow extends BaseJointWindow {
 	@Override public void apply(){
 		super.apply();
 		joint.setCenter(centerTable.getVertex());
+		joint.setMaxLength(Float.parseFloat(maxLengthField.getText()));
 	}
 
 	@Override public boolean clicked(Vector2 pos) {
