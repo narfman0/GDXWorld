91692e63bbbcaf6ec1acb950614627ec6866d40a
Jon Robison <narfman0@gmail.com>
2018-12-31 23:43:28 -0500

    -Fix missing JSPF and up some gradle related stuff. Auto cleanup test java files.

--- src/test/java/com/blastedstudios/gdxworld/ui/GDXRendererTest.java
+++ src/test/java/com/blastedstudios/gdxworld/ui/GDXRendererTest.java
@@ -9,26 +9,33 @@ import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.math.Vector2;
 
 public class GDXRendererTest {
-	private Camera camera;
-	
-	@Before public void setUp(){
-		camera = new CameraStub();
-	}
-	
-	@Test public void testFromParallax() {
-		camera.translate(1, 1, 0);
-		float scale = 2f;
-		Vector2 world = new Vector2(1,1);
-		Vector2 to = GDXRenderer.toParallax(scale, world, camera);
-		assertEquals(to.x, 1.5, .01);
-		assertEquals(to.y, 1.5, .01);
-		Vector2 from = GDXRenderer.fromParallax(scale, to, camera);
-		assertEquals(world.x, from.x, .01);
-		assertEquals(world.y, from.y, .01);
-	}
-	
-	private class CameraStub extends Camera{
-		@Override public void update() {}
-		@Override public void update(boolean arg0) {}
-	}
+    private Camera camera;
+
+    @Before
+    public void setUp() {
+        camera = new CameraStub();
+    }
+
+    @Test
+    public void testFromParallax() {
+        camera.translate(1, 1, 0);
+        float scale = 2f;
+        Vector2 world = new Vector2(1, 1);
+        Vector2 to = GDXRenderer.toParallax(scale, world, camera);
+        assertEquals(to.x, 1.5, .01);
+        assertEquals(to.y, 1.5, .01);
+        Vector2 from = GDXRenderer.fromParallax(scale, to, camera);
+        assertEquals(world.x, from.x, .01);
+        assertEquals(world.y, from.y, .01);
+    }
+
+    private class CameraStub extends Camera {
+        @Override
+        public void update() {
+        }
+
+        @Override
+        public void update(boolean arg0) {
+        }
+    }
 }
--- src/test/java/com/blastedstudios/gdxworld/world/quest/GDXQuestManagerTest.java
+++ src/test/java/com/blastedstudios/gdxworld/world/quest/GDXQuestManagerTest.java
@@ -22,91 +22,107 @@ import com.blastedstudios.gdxworld.world.quest.trigger.AbstractQuestTrigger;
 import com.blastedstudios.gdxworld.world.quest.trigger.IQuestTriggerInformationProvider;
 
 public class GDXQuestManagerTest {
-	private World world;
-	private GDXQuestManager manager;
-	private GDXLevel level1;
-	private GDXQuest quest1, quest2;
-	private Vector2 playerPosition;
-	
-	@Before public void setUp() throws Exception {
-		Gdx.app = mock(Application.class);
-		world = new World(new Vector2(), true);
-		playerPosition = new Vector2();
-		level1 = new GDXLevel();
-		level1.setName("level1");
-		quest1 = new GDXQuest();
-		quest1.setName("quest1");
-		quest1.setManifestation(new DialogManifestation("Quest 1 dialog", "Origin 1", "Type1"));
-		LinkedList<AbstractQuestTrigger> triggers1 = new LinkedList<>();
-		triggers1.add(new AABBTrigger(1, 1, 3, 3));
-		quest1.setTriggers(triggers1);
-		level1.getQuests().add(quest1);
-		quest2 = new GDXQuest();
-		quest2.setName("quest2");
-		quest2.setManifestation(new DialogManifestation("Quest 2 dialog", "Origin 2", "Type2"));
-		quest2.setPrerequisites("quest1");
-		LinkedList<AbstractQuestTrigger> triggers2 = new LinkedList<>();
-		triggers2.add(new AABBTrigger(4, 4, 6, 6));
-		quest2.setTriggers(triggers2);
-		level1.getQuests().add(quest2);
-		
-		IQuestManifestationExecutor executor = new IQuestManifestationExecutor() {
-			@Override public Body getPhysicsObject(String name) {
-				Array<Body> bodyArray = new Array<>(world.getBodyCount());
-				world.getBodies(bodyArray);
-				for(Body body : bodyArray)
-					if(name != null && name.equals(body.getUserData()))
-						return body;
-				return null;
-			}
-			@Override public Joint getPhysicsJoint(String name) {
-				Array<Joint> joints = new Array<>(world.getJointCount());
-				world.getJoints(joints);
-				for(Joint joint : joints)
-					if(joint.getUserData().equals(name))
-						return joint;
-				return null;
-			}
-			@Override public World getWorld() {
-				return world;
-			}
-		};
-		IQuestTriggerInformationProvider provider = new IQuestTriggerInformationProvider() {
-			@Override public boolean isDead(String name) {
-				return false;
-			}
-			@Override public Vector2 getPlayerPosition() {
-				return playerPosition;
-			}
-			@Override public boolean isNear(String origin, String target, float distance) {
-				Body originBody = null, targetBody = null;
-				Array<Body> bodyArray = new Array<>(world.getBodyCount());
-				world.getBodies(bodyArray);
-				for(Body body : bodyArray){
-					if(origin != null && origin.equals(body.getUserData()))
-						originBody = body;
-					if(target != null && target.equals(body.getUserData()))
-						targetBody = body;
-				}
-				return originBody.getPosition().dst(targetBody.getPosition()) < distance;
-			}
-			@Override public Body getPhysicsObject(String name) {
-				Array<Body> bodyArray = new Array<>(world.getBodyCount());
-				world.getBodies(bodyArray);
-				for(Body body : bodyArray)
-					if(name != null && name.equals(body))
-						return body;
-				return null;
-			}
-			@Override public boolean isAction() {
-				return false;
-			}
-		};
-		manager = new GDXQuestManager();
-		manager.initialize(provider, executor);
-	}
+    private World world;
+    private GDXQuestManager manager;
+    private GDXLevel level1;
+    private GDXQuest quest1, quest2;
+    private Vector2 playerPosition;
 
-	@Test public void testActive() {
+    @Before
+    public void setUp() throws Exception {
+        Gdx.app = mock(Application.class);
+        world = new World(new Vector2(), true);
+        playerPosition = new Vector2();
+        level1 = new GDXLevel();
+        level1.setName("level1");
+        quest1 = new GDXQuest();
+        quest1.setName("quest1");
+        quest1.setManifestation(new DialogManifestation("Quest 1 dialog", "Origin 1", "Type1"));
+        LinkedList<AbstractQuestTrigger> triggers1 = new LinkedList<>();
+        triggers1.add(new AABBTrigger(1, 1, 3, 3));
+        quest1.setTriggers(triggers1);
+        level1.getQuests().add(quest1);
+        quest2 = new GDXQuest();
+        quest2.setName("quest2");
+        quest2.setManifestation(new DialogManifestation("Quest 2 dialog", "Origin 2", "Type2"));
+        quest2.setPrerequisites("quest1");
+        LinkedList<AbstractQuestTrigger> triggers2 = new LinkedList<>();
+        triggers2.add(new AABBTrigger(4, 4, 6, 6));
+        quest2.setTriggers(triggers2);
+        level1.getQuests().add(quest2);
+
+        IQuestManifestationExecutor executor = new IQuestManifestationExecutor() {
+            @Override
+            public Body getPhysicsObject(String name) {
+                Array<Body> bodyArray = new Array<>(world.getBodyCount());
+                world.getBodies(bodyArray);
+                for (Body body : bodyArray)
+                    if (name != null && name.equals(body.getUserData()))
+                        return body;
+                return null;
+            }
+
+            @Override
+            public Joint getPhysicsJoint(String name) {
+                Array<Joint> joints = new Array<>(world.getJointCount());
+                world.getJoints(joints);
+                for (Joint joint : joints)
+                    if (joint.getUserData().equals(name))
+                        return joint;
+                return null;
+            }
+
+            @Override
+            public World getWorld() {
+                return world;
+            }
+        };
+        IQuestTriggerInformationProvider provider = new IQuestTriggerInformationProvider() {
+            @Override
+            public boolean isDead(String name) {
+                return false;
+            }
+
+            @Override
+            public Vector2 getPlayerPosition() {
+                return playerPosition;
+            }
+
+            @Override
+            public boolean isNear(String origin, String target, float distance) {
+                Body originBody = null, targetBody = null;
+                Array<Body> bodyArray = new Array<>(world.getBodyCount());
+                world.getBodies(bodyArray);
+                for (Body body : bodyArray) {
+                    if (origin != null && origin.equals(body.getUserData()))
+                        originBody = body;
+                    if (target != null && target.equals(body.getUserData()))
+                        targetBody = body;
+                }
+                return originBody.getPosition().dst(targetBody.getPosition()) < distance;
+            }
+
+            @Override
+            public Body getPhysicsObject(String name) {
+                Array<Body> bodyArray = new Array<>(world.getBodyCount());
+                world.getBodies(bodyArray);
+                for (Body body : bodyArray)
+                    if (name != null && name.equals(body))
+                        return body;
+                return null;
+            }
+
+            @Override
+            public boolean isAction() {
+                return false;
+            }
+        };
+        manager = new GDXQuestManager();
+        manager.initialize(provider, executor);
+    }
+
+    @Test
+    public void testActive() {
 //		TODO gradle upgrade errors
 //		manager.setCurrentLevel(level1);
 //		manager.tick(1f);
@@ -123,10 +139,12 @@ public class GDXQuestManagerTest {
 //		assertFalse(manager.isCompleted(quest2));
 //		manager.setStatus(quest2.getName(), CompletionEnum.COMPLETED);
 //		assertTrue(manager.isCompleted(quest2));
-	}
+    }
 
-	private float count = 0f;
-	@Test public void testRepeat() {
+    private float count = 0f;
+
+    @Test
+    public void testRepeat() {
 //		TODO gradle upgrade errors
 //		GDXQuest quest = new GDXQuest();
 //		quest.setManifestation(new AbstractQuestManifestation() {
@@ -158,5 +176,5 @@ public class GDXQuestManagerTest {
 //		assertTrue(manager.isCompleted(quest));
 //		assertTrue(manager.isActive(quest));
 //		assertEquals(2, count, 1e-6);
-	}
+    }
 }
